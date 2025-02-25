dad7d0e3ee9e42a15092556b2866d1689270d2ed
ahri <owlgrowl0v0@gmail.com>
2025-02-25 17:13:18 +0900

    show thumbnail and description update

--- shotgridAPI/shotgrid_connector.py
+++ shotgridAPI/shotgrid_connector.py
@@ -24,9 +24,10 @@ class ShotGridConnector:
         """특정 Task에 연결된 PublishedFile을 조회"""
         # PublishedFile 엔티티에서 Task ID를 기준으로 검색
         filters = [["task", "is", {"type": "Task", "id": task_id}]]
-        fields = ["id", "code", "path"]
+        fields = ["id", "code", "path", "description", "image", "created_at"]
+        order = [{"field_name": "created_at", "direction": "desc"}]  # 최신순 정렬
 
-        publishes = ShotGridConnector.sg.find("PublishedFile", filters, fields)
+        publishes = ShotGridConnector.sg.find("PublishedFile", filters, fields, order)
         publish_list = []
         
         # 퍼블리시 된 파일을 리스트로 리턴
@@ -36,10 +37,14 @@ class ShotGridConnector:
                 publish_dict = {
                     'id' : publish['id'],
                     'file_name' : publish['code'],
-                    'path' : publish['path']['url']
+                    'path' : publish['path']['url'],
+                    'description' : publish['description'],
+                    'thumbnail' : publish['image'],
+                    'created_at' : publish['created_at']
                 }
                 publish_list.append(publish_dict)
             return publish_list
+        
         # 테스크 안에 퍼블리시 된 파일이 없는 경우
         else:
             print(f"⚠ Task {task_id}에 연결된 퍼블리시 파일이 없습니다.")
