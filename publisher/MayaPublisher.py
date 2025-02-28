e61b6f3e6b9d30ce23d51f6e9f2fa1129d719748
rainymumu <hyerinyang@yonsei.ac.kr>
2025-02-28 16:41:28 +0900

    2 월 28일 작업 진행 공유

--- publisher/MayaPublisher.py
+++ publisher/MayaPublisher.py
@@ -1,23 +1,55 @@
 import os
 import datetime
+import shutil
 import maya.cmds as cmds
 from PublishPath import FilePath
+from publisher.convert_to_mov import FileConverter
 
-class MayaPublish():
-    
-    """Maya에서 Playblast 또는 Alembic을 저장하고 Mp4 또는 Mov로 변환하는 클래스"""
-    def __init__(self, output_dir="/nas/Viper/hyerin/Publisher"):
-        self.output_dir = output_dir
-        os.makedirs(self.output_dir, exist_ok=True)
+
+class MayaPublisher():
+
+    def __init__(self, task_type, asset_name=None, seq=None, shot=None):
+        self.task_type = task_type
+        self.asset_name = asset_name
+        self.seq = seq
+        self.shot = shot
+        self.publish_data = {} 
+
+    def publish(self):
+     # Task 유형에 맞는 퍼블리쉬 실행
+        if self.task_type in ["MDL", "RIG", "TXT"]:
+            self._publish_asset()
+        elif self.task_type in ["MM", "LAY", "ANM"]:
+            self._publish_shot()
+
+    def _publish_asset(self):
+        # MDL, RIG, TXT 퍼블리시 실행
+        publish_paths = FilePath.get_publish_path(self.task_type, {"asset_name": self.asset_name})
+
+        # Maya 씬 저장 (.ma)
+        cmds.file(rename=publish_paths["scene"])
+        cmds.file(save=True, type="mayaAscii")
+
+        # Alembic Cache 만들기
+        alembic_path = publish_paths["cache"]
+        cmds.AbcExport(j=f"-root |Asset -file {alembic_path}")
+
+        # Playblast 만들기
+        mov_path = publish_paths["mov"]
+        self._generate_playblast(mov_path)
+        print(f"{self.task_type} Publishing is completed!!")
+        self.publish_data = publish_paths # 결과 저장
         
-        # Alembic 플러그인 로드 확인
-        if not cmds.pluginInfo("AbcExport", query=True, loaded=True):
-            print("Alembic 플러그인이 로드되었습니다.")
-            
+    def _publish_shot(self):        
+        # MM, LAY, ANM 퍼블리쉬 실행
+        publish_paths = FilePath.get_publish_path(self.task_type, {"seq": self.seq, "shot": self.shot})
+        cmds.file(rename=publish_paths["scene"])
+        cmds.file(save=True, type="mayaAscii")
+
+        self.publish_data = publish_paths
+
     def save_alembic(self):
-        
-        """Alembic(.abc) 파일을 저장하는 함수(프레임 범위 없음)"""
-        
+        # 선택된 오브젝트의 Alembic(.abc) 파일을 저장
         selected_objects = cmds.ls(selection=True)
         alembic_path = "/nas/Viper/hyerin/test.abc"
 
@@ -27,6 +59,7 @@ class MayaPublish():
 
         # 선택된 오브제를 -root 옵션으로 설정
         objects_str = " ".join([f"-root {obj}" for obj in selected_objects])
+        alembic_command = f"{objects_str}"
 
         # Alembic Export 실행
         alembic_command = f"{objects_str} -file {alembic_path}"
@@ -69,27 +102,27 @@ class MayaPublish():
         # cmds.optionVar(intValue=("playblastFormat", 8))
         
         cmds.playblast(
-            format="qt",
-            filename=output_path,
-            sequenceTime=False,
-            clearCache=True,
-            viewer=False,
-            showOrnaments=False,
-            framePadding=4,
-            percent=100,
-            compression="jpeg",
-            quality=100,
-            forceOverwrite=True,
-            widthHeight=(1920, 1080),
-            startTime=1,
-            endTime=99
+            format="qt", #QuickTime(.mov)형식 "image"를 사용하면 프레임 이미지 시퀀스로 저장할 수도 있음
+            filename=output_path, # .mov 확장자를 붙이면 QuickTime 동영상 파일로 저장됨
+            sequenceTime=False, # True: 씬 타임라인 기준으로 시퀀스 출력, False: 현재 프레임 번호 기준 출력(일반적인playblast)
+            clearCache=True, # playblast 실행 전 cache 비움 True: 메모리 캐시 지우고, 새롭게 실행, False: 캐시 유지, 반복실행시 속도 빠름
+            viewer=False, # True: 파일 생성시 뷰어에서 자동 재생, False: 자동 실행 -> x (파일저장만)
+            showOrnaments=False, # 뷰포트 내 오버레이 UI(타임코드, 그리드 등) 표시 여부, True: 타임코드, FPS, HUD, False: Clean ver. Render
+            framePadding=4, # 파일명에 붙는 프레임 넘버의 자리수 지정
+            percent=100, # 출력 해상도 비율 조정
+            compression="jpeg", # 비디오 압축 방식 선택
+            quality=100, # 비디오 품질 설정 (0~100)
+            forceOverwrite=True, # 같은 이름 파일 덮어쓰기, False -> 오류 가능성 높아짐
+            widthHeight=(1920, 1080), # 출력 해상도 FHD, 3840x2160 -> 4K
+            startTime=1, # 시작 프레임 설정
+            endTime=99 # 종료 프레임 설정
         )
         print(f"Playblast 저장 완료: {output_path}")
 
     def set_viewport_option(self, option):
-        # 현재 활성화된 모델 패널을 가져옵니다
-        # panel = cmds.getPanel(withFocus=True) 원래 이건대
-        panel = "modelPanel4" # <-- 이거 테스트용
+        # 현재 활성화된 모델 패널을 가져옴
+        # panel = cmds.getPanel(withFocus=True)
+        panel = "modelPanel4" # <-- linux 에서 test용
         if option == "shaded":
             cmds.modelEditor(panel, edit=True, displayAppearance="smoothShaded", wireframeOnShaded=False, displayTextures=False)
         elif option == "wireframe on shaded":
@@ -108,21 +141,21 @@ class MayaPublish():
         scene_cameras = [cam for cam in all_cameras if "persp" not in cam and "top" not in cam and "side" not in cam and "front" not in cam]
 
         if not scene_cameras:
-            self.maeke_turntable()
+            self.make_turntable()
             all_cameras = cmds.ls(type="camera", long=True)
             scene_cameras = [cam for cam in all_cameras if "persp" not in cam and "top" not in cam and "side" not in cam and "front" not in cam]
 
         camera_to_use = scene_cameras[0]
         # panel = cmds.getPanel(withFocus=True)
         panel = "modelPanel4" # <-- 이거 테스트용
-        cmds.lookThru(panel, camera_to_use)
+        cmds.lookThru(panel, camera_to_use) # lookThru() 특정 뷰패널을 특정 카메라로 전환하는 명령어!
         cmds.viewFit(all=True) # 씬 전체에 맞게 뷰 핏
 
         print(f"뷰포트 카메라 설정 완료: {camera_to_use}")
 
-        output_path = FilePath.get_publish_path("Viper", "Character", "Hero_Character" , "MDL", version=1)
+        publish_paths = FilePath.get_publish_path("Viper", "Character", "Hero_Character" , "MDL", version=1) 
         
-        self.save_playblast(output_path)
+        self.save_playblast(publish_paths)
 
     def playblast_publish(self, options):
         """UI 옵션 리스트에 따라 Playblast 실행"""
@@ -130,17 +163,8 @@ class MayaPublish():
             self.set_viewport_option(option)
             self.export_playblast()
    
-
-# class UIPublisher():
-#     """UI를 통해 Maya에서 mp4 또는 mov 변환을 실행하는 클래스"""
-#     @staticmethod
-#     def export_from_maya(format_type):
-#         maya_pub = MayaPublish()
-#         maya_pub.publish_video(format_type)
-
-
 if __name__ == "__main__":
-    maya_pub = MayaPublish()
+    maya_pub = MayaPublisher()
     selected_options = ["shaded", "wireframe on shaded"]
     maya_pub.playblast_publish(selected_options)
     maya_pub.save_alembic()
